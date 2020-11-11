765d5007f4ea46c0e650e347f4c072844c4bff55
Mathew Labadze <mlabadze55@mgail.com>
2020-11-11 16:11:36 +0400

    pirveli davaleba

---
+++ pirveli davaleba/main.js
@@ -0,0 +1,7 @@
+// 1)დაწერეთ ფუნქცია რომელიც მიღებული რიცხვების მასივიდან
+//   დააბრუნებს უდიდეს ელემენტს
+
+function getMaxNumberArray(numbersArray) {
+    return Math.max(...numbersArray);
+  }
+  
